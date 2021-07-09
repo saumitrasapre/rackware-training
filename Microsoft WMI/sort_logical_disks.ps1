7680150e5b26935c68eaa03ad67ef820fe50ebb9
Saumitra <saumitrasapre69@gmail.com>
2021-07-09 21:00:05 +0530

    Modified sort_logical_disks script

--- Microsoft WMI/sort_logical_disks.ps1
+++ Microsoft WMI/sort_logical_disks.ps1
@@ -3,6 +3,7 @@ Write-Host "Computer: $env:COMPUTERNAME"
 
 $dsize = @{label = "DiskSize (GB)"; expression = {"{0:N}" -f ($_.Size/1GB) -as [float]}}
 $fspace = @{label = "FreeSpace (GB)"; expression = {"{0:N}" -f ($_.FreeSpace/1GB) -as [float]}}
+$ospace = @{label = "OccSpace (GB)"; expression = {"{0:N}" -f ($_.Size/1GB - $_.FreeSpace/1GB) -as [float]}}
 $percfree =  @{label = "% Free"; expression = {"{0:N}" -f ($_.FreeSpace/$_.Size * 100) -as [float]}}
 
 $physize = @{label = "Size (GB)"; expression = {"{0:N}" -f ($_.Size/1GB) -as [float]}}
@@ -19,8 +20,18 @@ $physize = @{label = "Size (GB)"; expression = {"{0:N}" -f ($_.Size/1GB) -as [fl
  Get-CimInstance -Namespace root/CIMV2 -ClassName Win32_LogicalDisk |`
     Where-Object {$_.DriveType -ne 5} |`
     Sort-Object -Descending -Property FreeSpace | `
-    Select-Object Name, VolumeName, FileSystem, Description, $dsize,$fspace, $percfree | `
+    Select-Object Name, VolumeName, FileSystem, Description, $dsize,$fspace, $ospace,$percfree | `
     Format-Table -AutoSize   
+#endregion  
+
+Write-Host "The following is the information about logical drives installed on this computer (Sorted according to occupied space)"
+Write-Host "Computer: $env:COMPUTERNAME"
+#region Logical Drive Info using Cim
+Get-CimInstance -Namespace root/CIMV2 -ClassName Win32_LogicalDisk |`
+Where-Object {$_.DriveType -ne 5} |`
+Sort-Object -Descending -Property @{Expression = {$_.Size - $_.FreeSpace}}| `
+Select-Object Name, VolumeName, FileSystem, Description, $dsize,$fspace,$ospace, $percfree | `
+Format-Table -AutoSize   
 #endregion  
 
 Write-Host "The following is the information about physical drives installed on this computer"
